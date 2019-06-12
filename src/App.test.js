0bf11fef878d954f7f12ba3c67368be7f7d7f4b4
Lepris <karolina.jankowska@gmail.com>
2019-06-12 12:43:34 +0100

    this testing tutorial

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { sum } from '../src/components/math';
 
 describe('Examining the syntax of Jest tests', () => {
-  it('sums numbers', () => {
-    expect(1 + 2).toEqual(3);
-    expect(2 + 2).toEqual(4);
+  test('sums numbers', () => {
+    expect(sum(1, 2)).toEqual(3);
+    expect(sum(2, 2)).toEqual(4);
   });
 });
